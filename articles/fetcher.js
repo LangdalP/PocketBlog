7ad80a7b4b01e48ec9821f9bf9752008d5481ade
Peder Voldnes Langdal <peder.langdal@gmail.com>
2016-08-10 13:15:58 +0200

    Added in-memory indexing of articles, and a file watcher

--- articles/fetcher.js
+++ articles/fetcher.js
@@ -1,22 +1,87 @@
 var fs = require('fs');
 
+/*
+ * Filenames are mapped to shorter identifiers (keys).
+ * The shorter identifiers are the ones used when readers
+ * request a single article, e.g. 'domain.com/posts/my-article'.
+ * 
+ * The keys are mapped to the content of the article
+ */
+var index = {
+    articleFilenames: {},
+    articleKeys: {}
+};
+
+function filenameToIndexKey(fname) {
+    var fnameParts = fname.split('_');
+    var indexLongName = fnameParts[fnameParts.length -1];
+    // Also trim the .md file extension
+    indexName = indexLongName.substring(0, indexLongName.length - 3);
+    return indexName;
+}
+
+function indexFile(path) {
+    var pathParts = path.split("/");
+    var fname = pathParts[pathParts.length - 1];
+    fs.readFile(path, 'utf8', function (err, data) {
+        if (err) throw err;
+        var indexName = filenameToIndexKey(fname);
+        index.articleKeys[indexName] = data;
+        index.articleFilenames[fname] = indexName;
+    });
+}
+
+function removeIndexedFile(path) {
+    var pathParts = path.split("/");
+    var fname = pathParts[pathParts.length - 1];
+    var indexName = filenameToIndexKey(indexName);
+    delete index.articleKeys[indexName];
+    delete index.articleFilenames[fname];
+}
+
 function tryFetchArticle(name, callback) {
-	var fname = 'articles/' + name + '.md';
-	fs.stat(fname, function(err, stat) {
-		if(err == null) {
-			fs.readFile(fname, 'utf8', function(err, data) {
-				if (err) throw err;
-				callback(data);
-			});
-		} else if(err.code == 'ENOENT') {
-			console.log('File ' + fname + ' does not exist!');
-			callback(null);
-		} else {
-			console.log('Some error happened while fetching article: ', err.code);
-			callback(null);
-		}
-	});
+    var data = index.articleKeys[name];
+    if (data) {
+        var lines = data.split(/\r?\n/);
+        var headlineRaw = lines[0];
+        var headline = headlineRaw.substring(2);
+        callback({
+            headline: headline,
+            articleMarkdown: data
+        });
+    }
+    else {
+        callback(null);
+    }
+}
+
+function tryFetchLatest(count, callback) {
+    var keys = Object.keys(index.articleFilenames);
+    if (count > keys.length) count = keys.length;
+    var sortedKeys = keys.sort();
+    var latestKeys = sortedKeys.slice(sortedKeys.length - count, sortedKeys.length);
+    var articles = [];
+    for (var i = 0; i < latestKeys.length; ++i) {
+        var key = filenameToIndexKey(latestKeys[i]);
+        var data = index.articleKeys[key];
+        if (data) {
+            var lines = data.split(/\r?\n/);
+            var headlineRaw = lines[0];
+            var headline = headlineRaw.substring(2);
+            articles.push({
+                headline: headline,
+                articleMarkdown: data
+            });
+        }
+        else {
+            console.log("Error happened while fetching latest articles");
+        }
+    }
+    callback(articles);
 }
 
 module.exports.tryFetchArticle = tryFetchArticle;
+module.exports.indexFile = indexFile;
+module.exports.removeIndexedFile = removeIndexedFile;
+module.exports.tryFetchLatest = tryFetchLatest;
 
---
+++ articles/watcher.js
@@ -0,0 +1,27 @@
+var chokidar = require('chokidar'); // For watching files and directories
+
+// Start listening, and report changes to the articles object passed in
+function start(articles) {
+    // Watch articles folder
+    var watcher = chokidar.watch('articles', {
+        ignored: 'articles/*.js',
+        persistent: true
+    });
+
+    // Use watcher to add new articles to index, and remove deleted articles
+    watcher
+        .on('add', path => {
+            console.log(`File ${path} has been added`);
+            articles.indexFile(path);
+        })
+        .on('change', path => {
+            console.log(`File ${path} has been changed`);
+            articles.indexFile(path);
+        })
+        .on('unlink', path => {
+            console.log(`File ${path} has been removed`);
+            articles.removeIndexedFile(path);
+        });
+}
+
+module.exports.start = start;
