fe5ba4202df78f65e5adf508ee4334008c15168f
Peder Voldnes Langdal <peder.langdal@gmail.com>
2016-08-29 14:34:56 +0200

    Trying to make the gulpfile more robust

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var fs = require('fs-extra')
+var path = require('path');
 
 var imageContentSrcPath = require('./config.json')['imageSource']
 var articleSrcPath = require('./config.json')['articleSource']
-var imageContentDstPath = 'images/content';
-var articleDstPath = 'content/articles';
+
+var workingDir = process.cwd();
+var imageContentDstPath = path.join(workingDir, 'images/content');
+var articleDstPath = path.join(workingDir, 'content/articles');
 
 gulp.task('images', function () {
 	// Can be customized
