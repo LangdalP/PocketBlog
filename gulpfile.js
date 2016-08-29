c01aca243644ab40eeecddd20fd0bcbcce2e7ab6
Peder Voldnes Langdal <peder.langdal@gmail.com>
2016-08-29 14:26:07 +0200

    Changed how content is added

--- gulpfile.js
+++ gulpfile.js
@@ -6,38 +6,20 @@ var articleSrcPath = require('./config.json')['articleSource']
 var imageContentDstPath = 'images/content';
 var articleDstPath = 'content/articles';
 
-gulp.task('clean-images', function () {
-	// Delete images
-	fs.remove(imageContentDstPath, function (err) {
-		if (err) return console.error(err);
-	});
-});
-
-gulp.task('clean-articles', function () {
-	// Delete articles
-	fs.remove(articleDstPath, function (err) {
-		if (err) return console.error(err);
-	});
-});
-
-gulp.task('clean', ['clean-images', 'clean-articles'], function () {
-	// clean-images and clean-articles are called as dependencies
-});
-
 gulp.task('images', ['clean-images'], function () {
 	// Can be customized
-	fs.copy(imageContentSrcPath, imageContentDstPath, function (err) {
-		if (err) return console.error(err);
-	});
+	fs.ensureLink(imageContentSrcPath, imageContentDstPath, function (err) {
+		if (err) return console.error(err)
+	})
 });
 
 gulp.task('articles', ['clean-articles'], function () {
 	// Can be customized
-	fs.copy(articleSrcPath, articleDstPath, function (err) {
-		if (err) return console.error(err);
-	});
+	fs.ensureLink(articleSrcPath, articleDstPath, function (err) {
+		if (err) return console.error(err)
+	})
 });
 
 gulp.task('default', ['images', 'articles'], function () {
-	console.log('Cleaning and fetching content');
+	console.log('Installing content links');
 });
